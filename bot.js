58f102731e2238b1d1cf0654be45d03798f34dc8
ZhouShangren <zhoushangren@outlook.com>
2020-04-07 14:19:59 +0800

    added auto select fishrod

--- bot.js
+++ bot.js
@@ -8,25 +8,93 @@ var client = mc.createClient({
     password: config.password
 });
 
-client.on('sound_effect', function(packet){
-    if(packet.soundId == 73){
-        client.write('use_item', {hand: 0});
-        console.log('fished!');
-        setTimeout(() => {
+client.on('sound_effect', function(packet) {
+    if(packet.soundId == 73){//咬钩
+        if(Do.getDistance(packet.x, packet.y, packet.z, Player.fishrod_x, Player.fishrod_y, Player.fishrod_z) / 8 <= config.fishrod_distance){
             client.write('use_item', {hand: 0});
-        }, 2000);
-    }
-});
-
-client.on('window_items', function(packet){
-    packet.items.forEach(item => {
-        if(item.present != false){
-            console.log(item);
+            console.log('上钩!');
+            setTimeout(() => {
+                client.write('use_item', {hand: 0});
+                Player.throw_time = new Date().getTime();
+            }, 2000);
+        }else{
+            console.log('远处的钓鱼声');
         }
-    });
+    }
+    if(packet.soundId == 258){//入水
+        var tnow = new Date().getTime();
+        if(tnow > Player.throw_time && tnow - Player.throw_time >= config.afterthrow_timeout){
+            Player.fishrod_x = packet.x;
+            Player.fishrod_y = packet.y;
+            Player.fishrod_z = packet.z;
+        }
+    }
+    
 });
 
-client.on('login', function(packet){
-    client.write('chat', {message: '爷 加入了游戏'});
-    client.write('use_item', {hand: 0});
+client.on('window_items', function(packet) {
+    try {
+        if(packet.items[Player.heldItemIndex].itemId == 622){
+            console.log('已持有鱼竿');
+            return;
+        }else{
+            console.log('拿了别的什么, 待切换至鱼竿');
+        }
+    } catch (error) {
+        console.log('两手空空, 待切换至鱼竿');
+    }
+    var haveFishrod = false;
+    packet.items.every(function(item, index){
+        if(item.present != false){
+            if(item.itemId == 622){
+                client.write('window_click', {
+                    windowId: packet.windowId,
+                    slot: index,
+                    mouseButton: 0,
+                    action: 1,
+                    mode: 2,
+                    item
+                });
+                haveFishrod = true;
+                return false;//break
+            }
+        }
+        return true;
+    });
+    if(!haveFishrod) console.log('注意! 没有在背包内找到钓鱼竿');
+    if(haveFishrod) console.log('已在背包内找到钓鱼竿并切换');
 });
+
+client.on('held_item_slot', function(packet){
+    Player.heldItemIndex = packet.slot + 36;
+})
+
+client.on('login', function(packet) {
+    console.log('已加入游戏');
+    setTimeout(() => {
+        client.write('use_item', {hand: 0});
+        Player.throw_time = new Date().getTime();
+    }, 3000);
+    Player.timer = setInterval(() => {
+        var tnow = new Date().getTime();
+        if(tnow > Player.throw_time && tnow - Player.throw_time >= config.auto_rethrow){
+            client.write('use_item', {hand: 0});
+            console.log('重抛');
+        }
+    }, 1000);
+});
+
+class Do {
+    static getDistance(x1, y1, z1, x2, y2, z2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+    }
+}
+class Player {
+    static heldItemIndex = 0;
+    static fishrod_x = 0;
+    static fishrod_y = 0;
+    static fishrod_z = 0;
+    static throw_time = 0;
+
+    static timer;
+}
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 //account and server config
-var server_host = 'yourserver.com';//server address
-var server_port = 25565;//server port
-var username = 'youremail@address.com';//your account email
-var password = 'yourpassword';//your password
+var server_host = 'yourserver.com';//服务器地址
+var server_port = 25565;//服务器端口
+var username = 'youremail@address.com';//账号邮箱
+var password = 'yourpassword';//账号密码
 
 exports.server_host = server_host;
 exports.server_port = server_port;
@@ -11,6 +11,10 @@ exports.password = password;
 
 ////////////
 
-var auto_rethrow = 30;//auto rethrow time (seconds)
+var auto_rethrow = 30000;//重抛超时。超过此时间还没有钓到鱼则尝试重新抛竿。 (毫秒)
+var fishrod_distance = 1;//浮标落水点与鱼咬钩点之间的距离 小于此值便会收杆
+var afterthrow_timeout = 1500;//抛竿与浮标落水的时间差，即抛竿后该段时间内的实体落水事件会被认为是浮标入水。服务器tps较低时需调高。 (毫秒)
 
 exports.auto_rethrow = auto_rethrow;
+exports.fishrod_distance = fishrod_distance;
+exports.afterthrow_timeout = afterthrow_timeout;
