3dd9c9a03ff950f1addfe789647d98c72bd6e741
ENTNEAZ <langyuez@outlook.com>
2021-10-12 23:22:03 +0800

    format output&english

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
+//encoding utf-8
+//todo : eat food
 var mc = require('minecraft-protocol');
 const { threadId } = require('worker_threads');
 var config = require('./config');
 
-var firstlogin = true;
-var loginhealth = 0;
+var firstLogin = true;
+var loginHealth = 0;
+
+var xpLevel = 0;
+var health = 20;
+var food = 20;
+var foodSat = 0;
 
 var client = mc.createClient({
     host: config.server_host,
@@ -15,23 +22,23 @@ var client = mc.createClient({
 
 client.on('sound_effect', function(packet) {
     if(packet.soundId == 369){//咬钩 1.15.2为73
-		var sound_distance = Do.getDistance(packet.x, packet.y, packet.z, Player.fishrod_x, Player.fishrod_y, Player.fishrod_z) / 8;
-		console.log(Do.getTime() + '[FishBot]Sound Distance: ' + sound_distance);
+		var sound_distance = (Do.getDistance(packet.x, packet.y, packet.z, Player.fishrod_x, Player.fishrod_y, Player.fishrod_z) / 8).toFixed(2);
+		Do.log('[FishBot]Sound Distance: ' + sound_distance);
         if(sound_distance <= config.fishrod_distance){
             client.write('use_item', {hand: 0});
-            console.log(Do.getTime() + '[FishBot]上钩!');
+            Do.log('[FishBot]Hooked!');
             setTimeout(() => {
                 client.write('use_item', {hand: 0});
                 Player.throw_time = new Date().getTime();
             }, 2000);
         }else{
-            console.log(Do.getTime() + '[FishBot]远处的钓鱼声');
+            Do.log('[FishBot]Far Fishing Sound');
         }
     }
     if(packet.soundId == 173){//入水 1.15.2为258
         var tnow = new Date().getTime();
         if(tnow > Player.throw_time && tnow - Player.throw_time <= config.afterthrow_timeout){//浮标入水后会持续产生入水音效 设置超时以避免重复计入
-			console.log(Do.getTime() + '[FishBot]浮标入水');
+			Do.log('[FishBot]Bobber in water');
             Player.fishrod_x = packet.x;
             Player.fishrod_y = packet.y;
             Player.fishrod_z = packet.z;
@@ -41,13 +48,13 @@ client.on('sound_effect', function(packet) {
 client.on('window_items', function(packet) {
     try {
         if(packet.items[Player.heldItemIndex].itemId == 797){ //1.15.2为622
-            console.log(Do.getTime() + '[FishBot]已持有鱼竿');
+            Do.log('[FishBot]Holding fishing rod');
             return;
         }else{
-            console.log(Do.getTime() + '[FishBot]拿了别的什么, 待切换至鱼竿');
+            Do.log('[FishBot]Changing to fishing rod');
         }
     } catch (error) {
-        console.log(Do.getTime() + '[FishBot]两手空空, 待切换至鱼竿');
+        Do.log('[FishBot]Changing to fishing rod');
     }
     var haveFishrod = false;
     packet.items.every(function(item, index){
@@ -67,15 +74,15 @@ client.on('window_items', function(packet) {
         }
         return true;
     });
-    if(!haveFishrod) console.log(Do.getTime() + '[FishBot]注意! 没有在背包内找到钓鱼竿');
-    if(haveFishrod) console.log(Do.getTime() + '[FishBot]已在背包内找到钓鱼竿并切换');
+    if(!haveFishrod) Do.log('[FishBot]Warning! No fishing rod in inventory');
+    if(haveFishrod) Do.log('[FishBot]Having changed item to fishing rod');
 });
 client.on('held_item_slot', function(packet){
     Player.heldItemIndex = packet.slot + 36;
 });
 
 client.on('login', function(packet) {
-    console.log(Do.getTime() + '[FishBot]已加入游戏');
+    Do.log('[FishBot]Joined in the game');
     setTimeout(() => {
         client.write('use_item', {hand: 0});
         Player.throw_time = new Date().getTime();
@@ -84,24 +91,28 @@ client.on('login', function(packet) {
         var tnow = new Date().getTime();
         if(tnow > Player.throw_time && tnow - Player.throw_time >= config.auto_rethrow){
             client.write('use_item', {hand: 0});
-            console.log(Do.getTime() + '[FishBot]重抛');
+            Do.log('[FishBot]Rethrow');
             Player.throw_time = tnow;
             client.write('use_item', {hand: 0}); 
         }
     }, 1000);
 });
+client.on('experience',function(packet){
+    xpLevel = packet.level;
 
+});
 client.on('update_health', function (packet) {
-    console.log(Do.getTime());
-    console.log(packet);
-    if (firstlogin){
-        loginhealth = packet.health;
-        firstlogin = false;
+    health = packet.health.toFixed(0);
+    food = packet.food.toFixed(0);
+    foodSat = packet.foodSaturation.toFixed(0);
+    if (firstLogin){
+        loginHealth = packet.health;
+        firstLogin = false;
     }
     
     if (packet.health < 10){
-        if (packet.health >= loginhealth){
-            console.log(Do.getTime() + "在恢复");
+        if (packet.health >= loginHealth){
+            Do.log("HP is recovering");
         }
         else{
             process.exit(0);//血量小于10但没在恢复 退出
@@ -111,27 +122,27 @@ client.on('update_health', function (packet) {
         }
     }
     
-  })
+  });
 
  client.on('kick_disconnect', function (packet) {
-    console.info(color('Kicked for ' + packet.reason, 'blink+red'));
+    Do.log('Kicked for ' + packet.reason);
     process.exit(1);
  });
   
   
  client.on('disconnect', function (packet) {
-    console.log(Do.getTime() + 'disconnected: ' + packet.reason)
+    Do.log('disconnected: ' + packet.reason)
     process.exit(1);
  });
   
  client.on('end', function () {
-    console.log(Do.getTime() + 'Connection lost')
+    Do.log('Connection lost')
     process.exit(1);
  });
   
  client.on('error', function (err) {
-    console.log(Do.getTime() + 'Error occured');
-    console.log(err);
+    Do.log('ERROR OCCURED,EXITING');
+    Do.log(err);
     process.exit(1);
  });
 
@@ -143,8 +154,17 @@ class Do {
 	
 	static getTime() {
         var now = new Date();
-		return '[' + now.getFullYear() + '/' + ( now.getMonth() + 1 ) + '/' + now.getDay() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + '] ';
+		return '[' + now.getFullYear() + '/' + ( now.getMonth() + 1 ) + '/' + now.getDay() + ' ' + ("0" + now.getHours()).slice(-2) + ':' + ("0" + now.getMinutes()).slice(-2) + ':' + ("0" + now.getSeconds()).slice(-2) + '] ';
 	}
+    static getXpLevel() {
+        return '[XpLevel:' + xpLevel +']';
+    }
+    static getHealthInf() {
+        return '[Health:' + health + ',Food:'+ food + ',FoodSat:' + foodSat + ']';
+    }
+    static log(log){
+        console.log(Do.getTime() + Do.getXpLevel() + Do.getHealthInf() + log);
+    }
 }
 
 class Player {
