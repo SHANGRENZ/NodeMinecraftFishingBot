6fe4c26ab2d7bc349b9c5dc229c64504dd8d0840
ENTNEAZ <langyuez@outlook.com>
2021-10-23 19:37:43 +0800

    issue #3 solved

--- config.js
+++ config.js
@@ -17,7 +17,11 @@ exports.auth = auth;
 var auto_rethrow = 40000;//重抛超时。超过此时间还没有钓到鱼则尝试重新抛竿。 (毫秒)
 var fishrod_distance = 25;//浮标落水点与鱼咬钩点之间的距离 小于此值便会收杆
 var afterthrow_timeout = 1500;//抛竿与浮标落水的时间差，即抛竿后该段时间内的实体落水事件会被认为是浮标入水。服务器tps较低时需调高。 (毫秒)
+var bobberDistance = 2;
+var bobberYVelocity = -0.1;
 
 exports.auto_rethrow = auto_rethrow;
 exports.fishrod_distance = fishrod_distance;
 exports.afterthrow_timeout = afterthrow_timeout;
+exports.bobberDistance = bobberDistance;
+exports.bobberYVelocity = bobberYVelocity;
